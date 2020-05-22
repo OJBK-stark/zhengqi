6d986138f14fe4c999b8ec312561801a5f8f8d7c
1273457900 <1273457900@qq.com>
2020-05-22 15:03:18 +0800

    添加了注释——LR
    添加了注释

--- app/app/src/main/java/ncist/wl171/no201707054121/app/MainActivity.java
+++ app/app/src/main/java/ncist/wl171/no201707054121/app/MainActivity.java
@@ -55,9 +55,9 @@ public class MainActivity extends AppCompatActivity {
 
     String requestUrl = "https://v1.alapi.cn/api/mingyan";//名言
     String requestUrl2 ="http://api.tianapi.com/generalnews/index";//新闻
-    TextView words ;
-    TextView News ;
-    Button refresh;
+    TextView words ;//定义名言显示的 textView
+    TextView News ;//定义新闻显示的 Textview
+    Button refresh;//刷新按钮
     String result;
 
     @Override
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        words = findViewById(R.id.textwords);
+        words = findViewById(R.id.textwords);//获取
         News = findViewById(R.id.textNews);
         refresh = findViewById(R.id.but_refresh);
 
@@ -73,16 +73,18 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        getWords("1");//获取名言
-        getNews("1");//获取新闻
+        getWords("1");//获取名言，参数没有什么意义
+        getNews("1");//获取新闻，参数没有什么意义
+
+        //鼠标监听事件
         refresh.setOnClickListener(new MyOnClickListener(){//第二种
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 super.onClick(v);
-                getWords("1");
-                Toast.makeText(MainActivity.this, "刷新", Toast.LENGTH_LONG).show();
+                getWords("1");//刷新 名言
+                Toast.makeText(MainActivity.this, "刷新", Toast.LENGTH_SHORT).show();
 
             }
         });
@@ -128,15 +130,15 @@ public class MainActivity extends AppCompatActivity {
 
            //提取JSON字符串中的相关信息
            // JSONObject jsonObject = JSONObject.fromObject(result);
-            JSONObject jsonObject = new JSONObject(result);
-            JSONObject jsonData = jsonObject.getJSONObject("data");
+            JSONObject jsonObject = new JSONObject(result);//提取JSON字符串中的相关信息
+            JSONObject jsonData = jsonObject.getJSONObject("data");//提取JSON字符串中的data  可以直接打开https://v1.alapi.cn/api/mingyan 看一下返回格式
 
-            String content = jsonData.getString("content");
-            String author = jsonData.getString("author");
-            result = "" +  content + " \n\n               —————" + author;
+            String content = jsonData.getString("content");//提取data 里的内容
+            String author = jsonData.getString("author");//提取data 里的作者
+            result = "" +  content + " \n\n               —————" + author;//按例输出
 
 
-            words.setText(filterHtml(result));
+            words.setText(filterHtml(result));//显示在textview——words里
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -156,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         //设置需要传递的参数
         //params.add(new BasicNameValuePair("type", "top"));
-        params.add(new BasicNameValuePair("key", "d502cd6dac51f1fc65741972c0fd14a1"));
+        params.add(new BasicNameValuePair("key", "d502cd6dac51f1fc65741972c0fd14a1"));//KEY 是从API提供商申请的免费 KEY
         //  params.add(new BasicNameValuePair("typeid", id));
 
         try {
@@ -190,14 +192,14 @@ public class MainActivity extends AppCompatActivity {
             JSONArray jsonArray = jsonObject.getJSONArray("newslist");
 
 
-            List<String> ctimes = new ArrayList<>();
-            List<String> titles = new ArrayList<>();
-            List<String> descriptions = new ArrayList<>();
-            List<String> picUrls = new ArrayList<>();
-            List<String> urls = new ArrayList<>();
+            List<String> ctimes = new ArrayList<>();//定义存储集合  时间
+            List<String> titles = new ArrayList<>();//标题
+            List<String> descriptions = new ArrayList<>();//简介
+            List<String> picUrls = new ArrayList<>();//图片url
+            List<String> urls = new ArrayList<>();//url                       PS：以上信息 均获取到了 目前使用了  title 和 url
 
             int j = 1;
-            for(int i=0;i<jsonArray.length();i++){
+            for(int i=0;i<jsonArray.length();i++){                            //JSON解析，
                 JSONObject partresult = jsonArray.getJSONObject(i);
                 String  ctime = partresult.getString("ctime");
                 ctimes.add(ctime);
@@ -211,7 +213,7 @@ public class MainActivity extends AppCompatActivity {
                 urls.add(url);
             }
             News.setText("");
-            for (int k=1;k<jsonArray.length();k++){
+            for (int k=1;k<jsonArray.length();k++){   //循环从集合取数据 设置到TextView——News里
                 //words.append(titles.get(k));
                 // words.append("\n\n");
                 // words.append(urls.get(k));
@@ -219,7 +221,7 @@ public class MainActivity extends AppCompatActivity {
                 String x=titles.get(k);
                 String y=urls.get(k);
                 News.append(
-                        Html.fromHtml(
+                        Html.fromHtml(//                                //使用Html.fromHtml设置超链接
                                 "<a href='"+y+"'>"+x+"</a> ")
                 );
                 News.append("\n\n");
@@ -240,7 +242,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    //使用正则表达式过滤XML文档中的标签（如<、>及空格等）
+    //使用正则表达式过滤XML文档中的标签（如<、>及空格等）           //暂未使用到
     private String filterHtml(String source) {
         if(null == source){
             return "";
